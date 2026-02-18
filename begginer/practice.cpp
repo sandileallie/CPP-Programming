c2267d0bf0ef4a8cf8cd4d8c98d66415346de104
sandileallie <87276003+sandileallie@users.noreply.github.com>
2026-02-18 17:42:28 +0200

    Update practice.cpp
    My first example from c++ from the textboob example page 45

--- begginer/practice.cpp
+++ begginer/practice.cpp
@@ -12,9 +12,28 @@ void practice(){
     }
 }
 
+void example_1(){
+
+/*
+Given length in inches, we write a program that determines and outputs the equivalent
+length in feet and (remaining) inches. Now there are 12 inches in a foot. Therefore, 100
+inches equals 8 feet and 4 inches; similarly, 55 inches equals 4 feet and 7 inches. Note
+that 100 / 12 = 8 and 100 % 12 = 4; similarly, 55 / 12 = 4 and 55 % 12 = 7. From these
+examples, it follows that we can effectively use the operators / and % to accomplish our
+task. The desired program is as follows:
+*/
+    int inches = 100;
+    int feet = inches/12;
+    int inche = inches%12;
+
+    std::cout << feet << " and " << inche;
+
+}
+
 int main(){
 
     practice();
+    example_1();
 
     return 0;
 }
