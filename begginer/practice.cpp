e95ee91224d02c8606b7c61a573bc7871192879c
SANDILE MASHABA <49841874@adt.nwu.ac.za>
2026-02-24 17:22:52 +0200

    lecture_03

--- begginer/practice.cpp
+++ begginer/practice.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#incluse <cmath>
 
 void practice(){
 
@@ -54,6 +55,85 @@ task. The desired program is as follows:
     int inche = inches%12;
 
     std::cout << feet << " and " << inche;
+
+}
+
+
+void class_exc(){
+
+    //2 to the power of 6
+    //12.5 to the power of 3
+    // sqr of 24
+    //the value of u
+    // length of a string
+
+
+    int power = pow(2,6);
+
+    std::cout << "The value of 2 to the power of 6 is : "
+         << power << '\n';
+
+
+    double sqr = pow(24.0, 0.5);
+    std::cout << "The value of 2 to the power of 6 is : "
+         << sqr << '\n';
+
+    int u = 24;
+    std::cout << "The value of u is : "
+         << u << '\n';
+
+    string name = "Sandile mashaba";
+    std::cout << "The lengt of string name = :" << name.length() << '\n';
+
+}
+
+
+void lecture_3(){
+
+    int x =25;
+    int y = 33;
+
+    std::cout << x / y << endl;
+    std::cout << "Hello there." << endl;
+    std::cout << 12 << endl;
+    std::cout << "x + y" << endl;
+    std::cout << 4 + 7 << endl;
+    std::cout << 'A' << endl;
+    std::cout << "x + y = " << 4 + 7 << endl;
+    std::cout << 2 + 3 * 5 << endl;
+    std::cout << "Hello \nthere." << endl;
+    std::cout << "Hello world!" << endl;
+
+}
+
+
+void revise(){
+    //***************************************************************
+    // Author: D.S. Malik
+    //
+    // This program shows where the include statements, using
+    // statement, named constants, variable declarations, assignment
+    // statements, and input and output statements typically appear.
+    //***************************************************************
+
+    int firstnum ;
+    int secondnum ;
+
+    firstnum = 18;
+    std::cout << "firstnum = " << firstnum << '\n';
+
+    std::cout << "Enter an integer : " <<'\n';
+    std::cin >> secondnum ;
+
+    std::cout << "Secondnum = " << secondnum <<'\n';
+
+    firstnum = firstnum + number + 2*secondnum;
+
+    std::cout << "the new value of"
+        << "firstnum = " << firstnum << '\n';
+
+
+
 
 }
 
